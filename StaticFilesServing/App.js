7d26983f93d5ce9b2977b76db84daef222a3420a
Typecaster12 <harshmishra2997@gmail.com>
2026-01-15 22:09:12 +0530

    Concept of Static file rendering

---
+++ StaticFilesServing/App.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+//if we have more than one static file(eg html, css, image, js files) and we have to render them;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//middleware;
+//this is the way to render static files(more than one, togeather);
+app.use(express.static(path.join(__dirname, "StaticFiles")));
+
+
+app.listen(3500, () => {
+    console.log('Server is running on 3500.');
+});
